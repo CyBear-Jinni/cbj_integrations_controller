fbd550a00dfe7dd1a33cbc39ab4ea426cc1940df
Guy Luz <guyluz11@gmail.com>
2023-12-01 22:33:12 +0200

    Inserted more logic that exists in the hub and the app

--- lib/domain/remote_pipes/i_remote_pipes_repository.dart
+++ lib/domain/remote_pipes/i_remote_pipes_repository.dart
@@ -1,8 +1,15 @@
 import 'package:cbj_integrations_controller/domain/remote_pipes/remote_pipes_entity.dart';
 import 'package:cbj_integrations_controller/domain/remote_pipes/remote_pipes_failures.dart';
+import 'package:cbj_integrations_controller/infrastructure/remote_pipes/remote_pipes_repository.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class IRemotePipesRepository {
+  static IRemotePipesRepository? _instance;
+
+  static IRemotePipesRepository get instance {
+    return _instance ??= RemotePipesRepository();
+  }
+
   Future<Either<RemotePipesFailures, Unit>> setRemotePipesDomainName(
     RemotePipesEntity remotePipesEntity,
   );
